7b3e5ab2e567bf7c55582848f4e2a16fea7c0611
Keven <hkeven89@gmail.com>
2026-03-09 22:50:16 -0400

    refactor

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,10 +5,10 @@ type CardProps = (typeof cards)[number]
 
 export default function Card({ title, description, icon, color }: CardProps) {
   return (
-    <div className={styles.root} style={{ "--card-color": color } as React.CSSProperties} >
+    <article className={styles.root} style={{ "--card-color": color } as React.CSSProperties} >
       <h2>{title}</h2>
       <p>{description}</p>
       <img src={icon} alt={title} />
-    </div>
+    </article>
   )
 }
